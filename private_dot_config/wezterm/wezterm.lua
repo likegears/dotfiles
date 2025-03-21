877444148951a5e33387ad87e996a18829be81e2
likegears <30487484+likegears@users.noreply.github.com>
2025-03-21 17:38:35 +0800

    Initial commit

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- Set the color scheme to Catppuccin Mocha
+config.color_scheme = 'Catppuccin Mocha'
+
+-- Set window background opacity to 50%
+config.window_background_opacity = 0.5
+
+-- Remove window decorations (title bar)
+config.window_decorations = "NONE"
+
+
+config.window_frame = {
+    -- The font used in the tab bar.
+    -- Roboto Bold is the default; this font is bundled
+    -- with wezterm.
+    -- Whatever font is selected here, it will have the
+    -- main font setting appended to it to pick up any
+    -- fallback fonts you may have used there.
+    font = wezterm.font { family = 'Comicshanns Nerd Font Mono', weight = 'Bold' },
+
+    -- The size of the font in the tab bar.
+    -- Default to 10.0 on Windows but 12.0 on other systems
+    font_size = 12.0,
+
+    -- The overall background color of the tab bar when
+    -- the window is focused
+    -- active_titlebar_bg = '#333333',
+
+    -- The overall background color of the tab bar when
+    -- the window is not focused
+    -- inactive_titlebar_bg = '#333333',
+}
+
+config.colors = {
+    tab_bar = {
+        -- The color of the inactive tab bar edge/divider
+        inactive_tab_edge = '#575757',
+    },
+}
+
+return config
